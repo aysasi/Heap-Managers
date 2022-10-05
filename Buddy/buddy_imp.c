44422c1928c613bc3d239c8294f65eb42b3df9e3
Alejandro Ysasi Cillero <alejandro.ysasi_cillero.6100@student.lu.se>
2022-10-05 12:21:49 +0100

    BUDDY WORKINGcd ..!

--- Buddy/buddy_imp.c
+++ Buddy/buddy_imp.c
@@ -3,23 +3,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stddef.h>
-#include <assert.h>
 
-
-#define MAX_ORDER 25		  // maximum order ;
+#define MAX_ORDER 30	  // maximum order ;
 #define MAX_SIZE (1 << MAX_ORDER) // total and maximum size = 2^K_MAX
-#define MIN_ORDER 4 //for blocks of order smaller or equal to 4 addresses where not stored correctly.
+#define MIN_ORDER 6  //for blocks of order smaller or equal to 4 addresses where not stored correctly
 
 
 //double linked list
 typedef struct dllist_t dllist_t;
 struct dllist_t
 {
-	unsigned int free :1; // width one field indicating if a block is being used or not
-	int order;	   // (2^order)
-	//size_t max_size; 	//maximux size that fits in a block
-	dllist_t *prev;	   // previous block in list of free nodes
-	dllist_t *next;	   // next block in list of free nodes
+	unsigned int free :1; 			// width one field indicating if a block is being used or not
+	int order;	   					// (2^order)
+	dllist_t *prev;	   				// previous block in list of free nodes
+	dllist_t *next;	   				// next block in list of free nodes
 };
 
 
@@ -84,8 +81,7 @@ void print_not_free_nodes() {
 }
 */
 
-void *malloc(size_t size)
-{
+void *malloc(size_t size) {
 
 	if (size == 0)
 		return NULL;
@@ -99,34 +95,21 @@ void *malloc(size_t size)
 	if (!head)		   // this is the first block to be allocated
 		init_memory(); // memory structure needs to be initilizated
 
-	//print_free_nodes();
-
 	int order = get_order(size + METADATA); // minimum order in which data fits
 	dllist_t *block = get_block(order);
 
-	if (block)
-	{ // not null, so a block has been found or created
+	if (block) { // not null, so a block has been found or created
 		block->free = 0;
+		remove_node(block);
 		return (block + 1);
 	}
 
 	return NULL;  //most likely to mean theres not more memory available
 }
 
-void *calloc(size_t nmemb, size_t size)
-{
 
-	/*
+void *calloc(size_t nmemb, size_t size) {	
 
-	size_t size = nbr_elements * element_size;
-	void* ptr = malloc(size);
-	if (ptr == NULL)
-		return NULL;
-	memset(ptr, 0, size);
-	return ptr;
-
-	*/
-	
 	if (nmemb == 0)
         	return NULL;
 
@@ -144,8 +127,8 @@ void *calloc(size_t nmemb, size_t size)
 	return NULL;
 }
 
-void *realloc(void *ptr, size_t size)
-{
+
+void *realloc(void *ptr, size_t size) {
 
 	if (ptr == NULL) {
 		return malloc(size);
@@ -156,64 +139,40 @@ void *realloc(void *ptr, size_t size)
 		return NULL;
 	}
 
-	int s = size;
-
-	dllist_t *block = (((dllist_t *)ptr) - 1);
-	if ((1 << block->order) >= s + METADATA) {  //if new size fits in the existing block, no need to create a new one
-		return ptr;
-	}
-
 	void *new_location = malloc(size);
 
-	if (new_location)
-	{
-		// if it is possible to allocate the data
-		//memcpy(new_location, ptr, (1 << block->order) - META_SIZE); // copies data from old region to new
-		dllist_t *new_block = (((dllist_t *)new_location) - 1);
-		memmove(new_location, ptr, (1 << new_block->order));
-		free(ptr);						 // frees old data
+	if (new_location) {
+		memmove(new_location, ptr, size);
+		free(ptr);						 				// frees old data
 		return new_location;
 	}
 
 	return NULL;
 }
 
-void free(void *ptr)
-{
+void free(void *ptr) {
 
-	if (ptr)
-	{
+	if (ptr) {
 		dllist_t *block = (((dllist_t *)ptr) - 1);
-		if (block->free == 0)
-		{
+		if (block->free == 0) {
 			block->free = 1;
 			merge(block);
 		}
 	}
 }
 
-void init_memory()
-{ // creates first block
+void init_memory() { // creates first block
 
 	int i = 0;
 	head = sbrk(MAX_SIZE);
 	
-	for (i = 0; i < MAX_ORDER; i++) {
-	    /*
-            void *aux = sbrk((1 << i));
-	    dllist_t *aux2 = aux;
-            aux2->free=0;
-            aux2->next=NULL;
-            aux2->order=i;
-            aux2->prev=NULL;
-	    free_nodes[i] = aux2;  */
+	for (i = 0; i < MAX_ORDER + 1; i++) {
 	    free_nodes[i] = NULL;
 	}
 
 	
 	dllist_t *first = head;
 	first->order = MAX_ORDER;
-	//first->max_size = (1 << MAX_ORDER);
 	first->free = 1;
 	first->next = NULL;
 	first->prev = NULL;
@@ -255,25 +214,16 @@ dllist_t *get_block(int order)
 	}
 
 	dllist_t *aux = free_nodes[order];  //error generated here
-    	dllist_t *bigger_block = NULL;
-	
-	while (aux != NULL) { //checks if there already exist a free block of that size
-	
-		if (aux->free == 1)
-			return aux; //if there already exists a free blck, it is returned
-		else {
-			if (aux->next != NULL)
-			    aux = aux->next;
-			else
-			    aux = NULL;
+
+	if (aux == NULL) {
+		dllist_t *bigger_block = NULL;
+		bigger_block = get_block(order + 1); //if it gets to this line, it means that there doesnt exist a (free) block of this size, so it needs to find a bigger one to be divided
+		if (bigger_block != NULL)
+		{ // it is necessary to split in halves these blocks
+			aux = half(bigger_block, order);
 		}
 	}
 
-	bigger_block = get_block(order + 1); //if it gets to this line, it means that there doesnt exist a (free) block of this size, so it needs to find a bigger one to be divided
-	if (bigger_block != NULL)
-	{ // it is necessary to split in halves these blocks
-		aux = half(bigger_block, order);
-	}
 	return aux;
 }
 
@@ -290,53 +240,41 @@ dllist_t *half(dllist_t *block, int new_order)
 		dllist_t *next_block = ((void *)block) + size;
 		next_block->order = changed_order;
 		next_block->free = 1;
-        next_block->next = NULL;
-        next_block->prev = NULL;
-
+        	next_block->next = NULL;
+        	next_block->prev = NULL;
+	
 		add_node(next_block);
 		add_node(block);
-		//DEspues de esta llamadaa el order de next block se cambia por algun motivo
-		//printf("·Hola");
 	}
 	return block;
 }
 
-void merge(dllist_t *block)
-{
+
+void merge(dllist_t *block) {
 	// the two posibilities to merge block with are:
 	dllist_t *left_bud = block->prev;
 	dllist_t *right_bud = block->next;
 	int x = 0;
-	if (left_bud != NULL)
-	{ // it is going to merge block with its left buddy
-		x = left_bud->free;
-		if (x == 1) {
-			if (left_bud->order == block->order) {
+	if (left_bud && left_bud->free == 1 && left_bud->order == block->order) { // it is going to merge block with its left buddy
+		remove_node(left_bud);
+		remove_node(block);
+		join_blocks(left_bud, block);
+		add_node(left_bud);
+		merge(left_bud);
 
-				remove_node(left_bud);
-				remove_node(block);
-				join_blocks(left_bud, block);
-				
-				//left_bud->order++;
-				add_node(left_bud);
-				merge(left_bud);
-			}
-		}
+		
 	}
-	else if (right_bud && right_bud->free && right_bud->order == block->order)
-	{
+	else if (right_bud && right_bud->free == 1 && right_bud->order == block->order) {
 		remove_node(block);
 		remove_node(right_bud);
 		join_blocks(block, right_bud);
 		add_node(block);
 		merge(block);	//checks if the now bigger block can be merged with other bigger blocks
-		//block->order++;
-
 	}
 }
 
-dllist_t *join_blocks(dllist_t *left, dllist_t *right)
-{
+
+dllist_t *join_blocks(dllist_t *left, dllist_t *right) {
 
 	dllist_t *merged_block = left;
 
@@ -354,27 +292,19 @@ dllist_t *join_blocks(dllist_t *left, dllist_t *right)
 	return merged_block;
 }
 
+
 void add_node(dllist_t *node)
 {
     if (node != NULL) {
 	int o = node->order;
 
-	// if (k == 4)
-	//	printf("%d\n", "Hola");
-
-	if (!free_nodes[o])
-	{ // there isnt any free node of this size yet
+	if (!free_nodes[o]) { // there isnt any free node of this size yet
 		free_nodes[o] = node;
 		node->next = NULL;
 		node->prev = NULL;
 	}
-	else
-	{ // we add the node to the existing nodes of this size
-        dllist_t *node_aux = node->prev;
-        while (node_aux != NULL) {
-            node = node_aux;
-            node_aux = node_aux->prev;
-        }
+
+	else { // we add the node to the existing nodes of this size
 
 		node->prev = NULL;
 		node->next = free_nodes[o];
@@ -390,32 +320,27 @@ void remove_node(dllist_t *node)
 
 	int o = node->order;
 
-	if (free_nodes[o] == node) { // node is going to be the first element in the list of free nodes
-		if (node->next == NULL) {
-		    free_nodes[o] = NULL;
-		}
-		else {
-		free_nodes[o] = node->next;
-		}
-	}
-
+	
 	// if they exist, the successor of node becomes the successor of the predecessor of node
 	if (node->prev)
 		node->prev->next = node->next;
 
 	// if they exist, the predecesor of node becomes the predecessor of the successor of node
-	if (node->next)
-		node->next->prev = node->prev;
+	if (node->next) {
+		if (free_nodes[o] == node)
+			free_nodes[o] = node->next;
 
+		node->next->prev = node->prev;
+	}
+
+	else {
+		if (free_nodes[o] == node)
+			free_nodes[o] = NULL;
+	}
+
+		
 	node->prev = NULL;
 	node->next = NULL;
 
 	}
 }
-/*
-int main() {
-
-    int *i = malloc(2);
-
-    return 0;
-}*/
